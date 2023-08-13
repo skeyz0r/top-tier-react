9a7b45efc3cdb6cc94285a8b7ecca43feb2cf50e
Shon Danvudi <sdanvudi@gmail.com>
2023-08-13 18:41:31 -0500

    Update Slideshow.js

--- react/app/Components/Slideshow.js
+++ react/app/Components/Slideshow.js
@@ -1,14 +1,5 @@
 'use client'
 
-import { useState } from "react"
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { Sarala } from "next/font/google"
-import Image from "next/image";
-
-const sarala = Sarala({
-  weight: '400',
-  subsets: ['latin'],
-})
 
 
 
@@ -18,7 +9,7 @@ export default function Slideshow() {
  
     
       return (
-        <section className="mb-[100px]">
+        <section>
        
         </section>
 
