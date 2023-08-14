d1bbbe475aaf965524e6d0f738b492ace9792005
Shon Danvudi <sdanvudi@gmail.com>
2023-08-13 19:14:58 -0500

    Services

--- react/app/page.js
+++ react/app/page.js
@@ -1,8 +1,10 @@
 import Main from "./Components/Main"
+import Services from "./Components/Services"
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Main/>
+      <Services/>
       </main>
       )}
