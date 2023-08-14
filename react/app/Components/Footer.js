ca3e9d6c40fff4328bef98f1de8c31976e4e0578
Shon Danvudi <sdanvudi@gmail.com>
2023-08-13 20:42:20 -0500

    Update Footer.js

--- react/app/Components/Footer.js
+++ react/app/Components/Footer.js
@@ -1,7 +1,15 @@
+import Link from "next/link"
 
 export default function Footer()
 {
     return(
-        <footer className={`w-full border-t  border-black border-solid h-16 bg-navYellow`}></footer>
+        <footer className={`w-full flex-col border-t justify-center items-center flex border-black border-solid bg-navYellow`}>
+            <div className="flex py-1 gap-2">
+                    <Link className="hover:underline" href={'/policy/privacy'}>Privacy Policy</Link>
+                    <p>|</p>
+                    <Link className="hover:underline" href={'/policy/conditions'}>Conditions of Use</Link>
+            </div>
+            <h2>Top Tier Services LLC ALL RIGHTS RESERVED</h2>
+        </footer>
     )
 }
