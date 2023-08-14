9583ebcd5458d0f7e56f5d5dad96b0708da1c686
Shon Danvudi <sdanvudi@gmail.com>
2023-08-14 13:30:10 -0500

    Update HouseService.js

--- react/app/Components/services/HouseService.js
+++ react/app/Components/services/HouseService.js
@@ -30,103 +30,25 @@ const sarala = Sarala({
 export default function House()
 {
     return(
-        <section className={`pt-20 flex flex-col`}>
-                <h1 className={`text-3xl ml-[4%] ${poppinsB.className} mt-10 `} >House Services</h1>
-                    <h2 className={` md:text-xl ml-[3%] ${poppinsL.className} mt-4 `}>Top Tier House and Handyman Services - Elevating Homes through Excellence</h2>
+        <section className={`pt-20 px-5 flex flex-col`}>
+                <h1 className={`text-3xl ml-[6%] ${poppinsB.className} mt-10 `} >House Services</h1>
+                    <h2 className={` md:text-xl ml-[4%] ${poppinsL.className} mt-4 mb-7 `}>Top Tier House and Handyman Services - Elevating Homes through Excellence</h2>
 
-        <div className="flex flex-wrap gap-5 my-9 ml-[2%]">
+        <div className="flex flex-col gap-5 my-9 ml-[2%]">
 
-        <div className="flex flex-col gap-7">
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Carpentry:</h2>
-                        <li>Furniture assembly</li>
-                        <li>Shelving Installation</li>
-                        <li>Trim and molding installation</li>
-                        <li>Door a window repairs</li>
-                    </ul>
-
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Plumbing:</h2>
-                        <li>Faucet and fixture repairs/replacement</li>
-                        <li>Toilet repairs</li>
-                        <li>Leaky pipe fixing</li>
-                        <li>Drain cleaning</li>
-                    </ul>
-           
-
-
-            <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Roofing & Siding</h2>
-                        <li>Roof leak repairs</li>
-                        <li>Siding maintenance</li>
-                        <li>Gutter repair</li>
-                    </ul>     
-                  
-
-
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Home Maintenance:</h2>
-                        <li>Gutter Cleaning</li>
-                        <li>Power Washing</li>
-                        <li>Weatherproofing</li>
-                        <li>Pest control</li>
-                    </ul>   
-
-
-<ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>General Repair:</h2>
-                        <li>Patching holes in walls</li>
-                        <li>Fixing squeaky doors/floors</li>
-                        <li>Repairing damaged tiles</li>
-                        <li>Lock and handle replacements</li>
-                    </ul>      
-</div>
-
-                    <div className="flex flex-col gap-5">
-
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Deck & Patio:</h2>
-                        <li>Deck repairs</li>
-                        <li>Patio installation</li>
-                        <li>Staining/sealing</li>
-                        <li>Railing installation</li>
-                    </ul>        
-
-
-
-<ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Tile & Flooring</h2>
-                        <li>Tile installation</li>
-                        <li>Grout cleaning/replacement</li>
-                        <li>Hardwood/laminate floor repairs</li>
-                    </ul>      
-
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Window & Screen:</h2>
-                        <li>Window repairs</li>
-                        <li>Screen replacement</li>
-                        <li>Weatherstripping</li>
-                    </ul>        
-                
-
-<ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Drywall & Plasters</h2>
-                        <li>Drywall installation/repair</li>
-                        <li>Plaster patching</li>
-                        <li>Texturing walls</li>
-                    </ul>      
-
-                    <ul className={`${comfortaa.className} flex gap-1 flex-col mb-9 list-disc px-10`}>
-                    <h2 className={`${sarala.className} mb-3`}>Painting:</h2>
-                        <li>Interior and exterior painting</li>
-                        <li>Touch-up work</li>
-                        <li>Drywall repairs</li>
-                        <li>Staining and varnishing</li>
-                    </ul>      
-                    </div>
-
-
-        </div>            
+                    <h3 className={`${poppinsL.className} text-xl`}>List of Top Tier House Services:</h3>
+        <ul className={`${comfortaa.className} flex gap-1 flex-col pl-[3%] mb-9 list-disc px-10`}>
+    <li>Roofing and Siding</li>
+    <li>Gutter Repair and Cleaning</li>
+    <li>Power Washing</li>
+    <li>Wall Holes Patching</li>
+   <li>Fixing Doors/Floors</li>
+    <li>Repairing and Installing Tiles</li>
+    <li>Drywall Installation/repair</li>
+                        <li>Plaster Patching</li>
+                        <li>Touch ups</li>
+                        <li>And any more additional service!</li>
+</ul>        </div>            
         </section>
     )
 }
