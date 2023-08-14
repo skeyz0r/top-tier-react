479e8b4748d32c36be5201bbb33a30ff306ea51a
Shon Danvudi <sdanvudi@gmail.com>
2023-08-13 19:30:46 -0500

    Footer

---
+++ react/app/Components/Footer.js
@@ -0,0 +1,7 @@
+
+export default function Footer()
+{
+    return(
+        <footer className={`w-full border-t  border-black border-solid h-16 bg-navYellow`}></footer>
+    )
+}
--- react/app/page.js
+++ react/app/page.js
@@ -1,6 +1,7 @@
 import Main from "./Components/Main"
 import Services from "./Components/Services"
 import Slideshow from "./Components/Slideshow"
+import Footer from "./Components/Footer"
 
 export default function Home() {
   return (
@@ -8,5 +9,6 @@ export default function Home() {
       <Main/>
       <Services/>
       <Slideshow/>
+      <Footer/>
       </main>
       )}
