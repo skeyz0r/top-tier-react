7742122598865e0173ff4bd8a2a50f0dc10a30e2
Shon Danvudi <sdanvudi@gmail.com>
2023-08-13 19:46:57 -0500

    Update Slideshow.js

--- react/app/Components/Slideshow.js
+++ react/app/Components/Slideshow.js
@@ -46,14 +46,14 @@ export default function Slideshow() {
         <section className="mb-[100px]">
         <div className='max-w-[1000px] h-[730px] w-full m-auto py-16 px-4 relative group'>
         <h1 className={`${sarala.className} text-6xl  mb-4`}>Gallery</h1>
-          <div className="flex items-center">
-            <BsChevronCompactLeft className="cursor-pointer" onClick={left} size={30}/>
+          <div className="flex items-center transition-all">
+            <BsChevronCompactLeft color="white" className="absolute rounded-lg bg-black left-4 md:left-0 md:relative cursor-pointer" onClick={left} size={30}/>
             <Image className="w-full object-contain h-[500px]"
             src={`/slides/${img}.jpg`}
               width={500}
               height={500}
             />
-            <BsChevronCompactRight className="cursor-pointer border border-solid border-black" onClick={right} size={30}/>
+            <BsChevronCompactRight color="white" className="absolute right-4 md:right-0 md:relative cursor-pointer border border-solid rounded-lg bg-black border-black" onClick={right} size={30}/>
           </div>
         </div>
         </section>
