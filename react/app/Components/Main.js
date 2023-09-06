02904d9856d3d9e3d0f9e0b0b22fba79eca85214
Shon Danvudi <sdanvudi@gmail.com>
2023-09-06 13:28:57 -0500

    fre q

--- react/app/Components/Main.js
+++ react/app/Components/Main.js
@@ -12,17 +12,21 @@ const inder = Comfortaa({
 
 export default function Main() {
   return (
-    <div className="flex pt-[50px]  w-full">
+    <div className="flex pt-[50px] w-full">
       <Image
       alt="backMainImg"
         className="w-full min-h-[150px]"
-        src={'/header.jpg'}
+        src={'/head.png'}
         width={1920}
         height={1080} />
-   <div id="btnCont" className="flex justify-end absolute w-[40%] self-end mb-6">
-      <button className="btn">
-    Call us Now
-</button>      
+   <div id="btnCont" className="flex flex-col gap-2 md:gap-10 ml-[30px] md:ml-[12%] absolute items-center w-fit self-center">
+    <h2 className={`w-fit text-xl md:text-2xl lg:text-3xl md:mb-20`}>The help you were looking for!</h2>
+   <button className="btn w-fit">
+    <p className={`md:text-xl lg:text-2xl`}>GET FREE QUOTE</p>
+    <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg">
+        <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-linejoin="round" stroke-linecap="round"></path>
+    </svg>
+</button>    
 </div>
 
     
