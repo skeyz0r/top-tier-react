7f64e17bb820b1d512876a155a13ec1a99aa5f5f
Shon Danvudi <sdanvudi@gmail.com>
2023-09-13 09:59:20 -0500

    w

--- react/app/Components/Navigator.js
+++ react/app/Components/Navigator.js
@@ -64,7 +64,7 @@ const navi = (prop) => {
 
   return (
     <nav className={`flex h-[56.8px] justify-between fixed w-full z-30 items-center border-solid border-b bg-navYellow border-gray-600`}>
-            <Link className={`${poppins.className} ${logo} logo pl-7 text-green-600 md:text-2xl`} href={'/'}>Top Tier Services LLC</Link>
+            <Link className={`${poppins.className} ${logo} logo pl-7 text-green-600 md:text-2xl`} href={'/'}>Top Tier Services</Link>
                     <div className={` ${vis}  flex items-center gap-3 md:gap-5 sm:mr-7 `}>
                     <h3 onClick={() => {   path.includes('/services') ? router.push('/') : srv.scrollIntoView({ behavior: "smooth", block: "start" });}} className={`${comf.className} hidden md:block  sm:${vis} cursor-pointer text-green-600 lg:text-xl py-3`}>Services</h3>
                     <h3 onClick={() => {   path.includes('/services') ? router.push('/') : cont.scrollIntoView({ behavior: "smooth", block: "start" });}} className={`${comf.className} hidden md:block  sm:${vis} cursor-pointer text-green-600 lg:text-xl py-3`}>Contact</h3>
